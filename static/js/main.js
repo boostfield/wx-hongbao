ac5d676a3c6b12d44ec7eea030595e27c20fb57d
qky1412 <qky1412@gmail.com>
2016-11-14 10:34:34 +0800

    remove background image temporarily

--- static/js/main.js
+++ static/js/main.js
@@ -184,8 +184,8 @@ function generateShareImage() {
 	var imageQRWidth = 160/375*screen.width;
 
 	var ctx = canvas.getContext("2d");
-	ctx.drawImage(imageBackground, 0, 0, canvas.width, canvas.height);
-	ctx.drawImage(imageBG, (canvas.width - imageBGWidth) / 2, (canvas.height - imageBGHeight) / 2, imageBGWidth, imageBGHeight);
+	//ctx.drawImage(imageBackground, 0, 0, canvas.width, canvas.height);
+	//ctx.drawImage(imageBG, (canvas.width - imageBGWidth) / 2, (canvas.height - imageBGHeight) / 2, imageBGWidth, imageBGHeight);
 	ctx.drawImage(imageQR,  (canvas.width - imageQRWidth) / 2, (canvas.height - imageQRWidth) / 2, imageQRWidth, imageQRWidth);
 	var dataURL = canvas.toDataURL();
 	alert("dataURL = " + dataURL);
